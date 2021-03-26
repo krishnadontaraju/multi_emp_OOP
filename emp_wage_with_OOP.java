ec42ad425b42d27b5ce3fcd2fde47226d635902f
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 07:32:40 +0530

    added the daily wage

--- emp_wage_with_OOP.java
+++ emp_wage_with_OOP.java
@@ -1,2 +1,42 @@
-//welcom
+import java.util.Random;
 
+public class employee_wage_with_OOP {
+
+    public static void main(String[] args) {
+        // Creating an Instance of class Employee_check
+        Employee_check first_emp = new Employee_check();
+        // Calling the attendance method on first_emp
+        first_emp.attendance();
+
+
+    }
+
+    //Defining the class Employee_check
+    public static class Employee_check {
+
+        Employee_check(/*int i, int j*/){
+            //Initializing the wage and working hours
+            int working_hours = 8;
+            int wage_per_hr = 20;
+
+        }
+
+        Random check = new Random();
+        //Defining attendacne method
+        public void attendance() {
+
+            
+
+            int checker =check.nextInt(2);
+            //Marking the attendance of the employee
+            if (checker == 1)
+                System.out.println("THE EMPLOYEE IS PRESENT");
+            else
+                System.out.println("THE EMPLOYEE IS ABSENT");    
+            
+        }
+
+    }
+
+    
+}
