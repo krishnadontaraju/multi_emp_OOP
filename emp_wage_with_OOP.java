257a3736ef4496921c6b11a11bde12e010a26a4c
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-26 07:28:06 +0530

    marked the attendance with random

--- emp_wage_with_OOP.java
+++ emp_wage_with_OOP.java
@@ -1,2 +1,35 @@
-//welcom
+import java.util.Random;
 
+public class employee_wage_OOP{
+
+    public static void main(String[] args) {
+        // Creating an Instance of class Employee_check
+        Employee_check first_emp = new Employee_check();
+        // Calling the attendance method on first_emp
+        first_emp.attendance();
+
+
+    }
+
+    //Defining the class Employee_check
+    public static class Employee_check {
+
+        Random check = new Random();
+        //Defining attendacne method
+        public void attendance() {
+
+            
+
+            int checker =check.nextInt(2);
+            //Marking the attendance of the employee
+            if (checker == 1)
+                System.out.println("THE EMPLOYEE IS PRESENT");
+            else
+                System.out.println("THE EMPLOYEE IS ABSENT");    
+            
+        }
+
+    }
+
+
+}
